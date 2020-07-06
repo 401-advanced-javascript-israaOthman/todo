58665c97c3af2a75f8ae5e6ecc0db0ccaef81630
israaothman <israaayoub@hotmail.com>
2020-07-07 02:36:56 +0300

    done lab32

--- src/components/todo-connected.js
+++ src/components/todo-connected.js
@@ -2,6 +2,11 @@ import React, { useEffect, useState } from 'react';
 import TodoForm from './form.js';
 import TodoList from './list.js';
 import useAjax from './hooks/useAjax';
+import Header from './header.js';
+import Row from 'react-bootstrap/Row';
+import Container from 'react-bootstrap/Container';
+import Col from 'react-bootstrap/Col';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './todo.scss';
 
 
@@ -14,26 +19,31 @@ const ToDo = () => {
 
   return (
     <>
-      <header>
-        <h2>
-          There are {list.filter(item => item.complete == 'pending').length} Items To Complete
-        </h2>
+    <header>
+        <Header />
       </header>
-
-      <section className="todo">
-
-        <div>
+      <Container>
+        <Row className="justify-content-md-center">
+          <Col><h2>
+          There are {list.filter(item => item.complete == 'pending').length} Items To Complete
+        </h2></Col>
+        </Row>
+        <Row className="todo">
+          <Col className="form"> 
+           <div>
           <TodoForm handleSubmit={_addItem} />
-        </div>
-
-        <div>
+        </div></Col>
+          <Col >
+          <div>
           <TodoList
             list={list}
             handleComplete={_toggleComplete}
             handleDelete={_deleteItem}
           />
         </div>
-      </section>
+          </Col>
+        </Row>
+      </Container>
     </>
   );
 };
