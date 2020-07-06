5bce3a0936a5547e76057d0e083e734420ac69a0
israaothman <israaayoub@hotmail.com>
2020-07-06 22:54:26 +0300

    custom hooks

--- src/app.js
+++ src/app.js
@@ -1,13 +1,13 @@
 import React from 'react';
 
 import ToDo from './components/todo.js';
-
+import Todoconnected from './components/todo-connected'
 
 export default class App extends React.Component {
   render() {
     return (
       <>
-        <ToDo />
+        <Todoconnected />
       </>
     );
   }
--- src/components/list.js
+++ src/components/list.js
@@ -2,19 +2,21 @@ import React, { useState, useEffect } from 'react';
 import ListGroup from 'react-bootstrap/ListGroup';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-function TodoList({list,handleComplete}){
+function TodoList({list,handleComplete,handleDelete}){
   console.log('ddd',list); 
 
   return (
     <ListGroup className="ul">
       {list.map(item => (
         <ListGroup.Item
-          className={`complete-${item.complete.toString()} li ` }
+          className={`complete-${item.complete} li ` }
           key={item._id}
         >
           <span onClick={() => handleComplete(item._id)}>
-            {item.text}  By   {item.assignee} 
+            {item.text}  ---- {item.complete} ---- {item.assignee} 
           </span>
+          <button onClick={() => handleDelete(item._id)}>x</button>
+
           </ListGroup.Item>
       ))}
     </ListGroup>
