1bf073642ba620077c0dd230d075527c55ba407d
israaothman <israaayoub@hotmail.com>
2020-07-07 21:19:04 +0300

    add showHide button

--- src/components/list.js
+++ src/components/list.js
@@ -1,18 +1,18 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useContext } from 'react';
 import ListGroup from 'react-bootstrap/ListGroup';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import {ToggleContext} from './context/hideShow';
 
 function TodoList({list,handleComplete,handleDelete}){
-  // console.log('ddd',list); 
-
+ const toggleContext = useContext(ToggleContext);
   return (
     <Container  className="ul">
        {list.map(item => (
-         <Container className="li"  key={item._id}>
+         <Container className={` complete-${item.complete}-${toggleContext.status} li`}  key={item._id}>
             <Row className="firstRow">
                 <Col  key={item._id} className={`complete-${item.complete}`}  onClick={() => handleComplete(item._id)}>
                    {item.complete}
--- src/components/todo-connected.js
+++ src/components/todo-connected.js
@@ -1,65 +1,76 @@
 import React, { useEffect, useState } from 'react';
 import TodoForm from './form.js';
 import TodoList from './list.js';
-import Pagination from './pajination';
+import Pagination from './pagination';
 import useAjax from './hooks/useAjax';
 import Header from './header.js';
+import ToggleShowProvider from './context/hideShow';
+import ToggleHideShow from './toggleHideShow';
+
 import Row from 'react-bootstrap/Row';
 import Container from 'react-bootstrap/Container';
 import Col from 'react-bootstrap/Col';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './todo.scss';
+import ToggleStatusHideShow from './context/hideShow';
 
 
 
 const ToDo = () => {
 
-  const [_addItem,_toggleComplete,_getTodoItems,_deleteItem,list] = useAjax();
-  const [itemsPerPage,setItemsPerPage] = useState(3);
+  const [_addItem, _toggleComplete, _getTodoItems, _deleteItem, list] = useAjax();
+  const [itemsPerPage, setItemsPerPage] = useState(3);
   const [currentPage, setCurrentPage] = useState(1);
 
 
   useEffect(_getTodoItems, []);
 
- // Get current posts
- const indexOfLastItem = currentPage * itemsPerPage;
- const indexOfFirstItem = indexOfLastItem - itemsPerPage;
- const currentItems = list.slice(indexOfFirstItem, indexOfLastItem);
+  // Get current posts
+  const indexOfLastItem = currentPage * itemsPerPage;
+  const indexOfFirstItem = indexOfLastItem - itemsPerPage;
+  const currentItems = list.slice(indexOfFirstItem, indexOfLastItem);
 
- // Change page
- const paginate = pageNumber => setCurrentPage(pageNumber);
+  // Change page
+  const paginate = pageNumber => setCurrentPage(pageNumber);
 
   return (
     <>
-    <header>
+      <header>
         <Header />
       </header>
       <Container>
         <Row className="justify-content-md-center">
           <Col><h2>
-          There are {list.filter(item => item.complete == 'pending').length} Items To Complete
+            There are {list.filter(item => item.complete == 'pending').length} Items To Complete
         </h2></Col>
         </Row>
         <Row className="todo">
-          <Col className="form"> 
-           <div>
-          <TodoForm handleSubmit={_addItem} />
-        </div></Col>
+          <Col className="form">
+            <div>
+              <TodoForm handleSubmit={_addItem} />
+            </div></Col>
           <Col >
-          <div>
-          <TodoList
-            list={currentItems}
-            handleComplete={_toggleComplete}
-            handleDelete={_deleteItem}
-          />
-        </div>
-          </Col>
-          <Col>
-          <Pagination 
-          itemsPerPage={itemsPerPage}
-          totalItems={list.length}
-          paginate={paginate}
-          />
+            <div>
+            <ToggleShowProvider>
+              <ToggleHideShow />
+               
+                  <TodoList
+                    list={currentItems}
+                    handleComplete={_toggleComplete}
+                    handleDelete={_deleteItem}
+                  />
+                
+            </ToggleShowProvider>
+            </div>
+            <Row >
+              <Pagination
+                itemsPerPage={itemsPerPage}
+                totalItems={list.length}
+                paginate={paginate}
+                currentPage={currentPage}
+
+              />
+            </Row>
           </Col>
         </Row>
       </Container>
